d7d7cc6ab8cf642eeaa9713371a88bba16d37240
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-04-26 21:28:28 -0500

    Updating README and environment

--- enviroment.rb
+++ enviroment.rb
@@ -2,9 +2,9 @@ require "rubygems"
 require "bundler"
 require "yaml"
 
-ENV["RACK_ENV"] ||= "development" unless ENV["RACK_ENV"]
+ENV["RACK_ENV"] ||= "development"
 AppConfig = YAML.load_file(File.join(File.dirname(__FILE__),'app_config.yml'))[ENV['RACK_ENV']]
-ENV["MONGOHQ_URL"] ||= AppConfig['mongo_url'] unless ENV["MONGOHQ_URL"]
+ENV["MONGOH_URL"] ||= ENV["MONGOHQ_URL"] || AppConfig['mongo_url']
 
 Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 
